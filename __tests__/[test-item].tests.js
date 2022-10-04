45d9bd01d4e43899a23b3e55d5186f338425fe7b
Quin-Riley-A <quin.asselin1@gmail.com>
2022-10-03 20:52:53 -0400

    include rest of packages from standard environment

---
+++ __tests__/[test-item].tests.js
@@ -0,0 +1,12 @@
+import [test item] from './../src/[test-item].js'
+
+describe('[test-item]', () => {
+  test('should [do a thing stated here]', () => {
+    //code
+    const date = new Date('2022-10-01');
+    //expect statements
+    expect([thing]).toEqual([other things]);
+    expect(date.month).toEqual("10");
+    expect(date.day).toEqual("01");
+  });
+});
